7c423aa90b7a4562756d65de4ce15d35c551d8ab
raudiver <mrhunterdi@gmail.com>
2017-06-01 00:33:28 +0300

    Added "while" 1) need to fix bug when enetered not 1,2,3,4 2) need to fix bug if char was enetred when calc asking for choose operation

--- simple_calc/main.cpp
+++ simple_calc/main.cpp
@@ -9,41 +9,47 @@ int main ()
     double razn(0.0); // results of operation subtraction
     double pow(0.0); // results of operation multiplication
     double div(0.0); // results of operation division
+    char continueargument ('y'); // User choice for continue using calculator
     std::cout << "First calculator!!!" << std::endl;
-    std::cout << "Choose action:" << std::endl << "Enter 1 for +" << std::endl << "Enter 2 for -" << std::endl << "Enter 3 for *" << std::endl << "Enter 4 for / " << std::endl;
-    std::cin >> operationtype;
-    std::cout << "Enter first number." << std::endl;
-    std::cin >> a1;
-    std::cout << "Eenter second number." << std::endl;
-    std::cin >> a2;
-    switch (operationtype) {
-    case 1:
-        {
-        sum = a1 + a2;
-        std::cout << "Sum is " << sum << std::endl;
-        break;
-        }
-    case 2:
-        {
-        razn = a1 - a2;
-        std::cout << "Difference is " << razn << std::endl;
-        break;
-        }
-    case 3:
-        {
-        pow = a1 * a2;
-        std::cout << "Product is " << pow << std::endl;
-        break;
-        }
-    case 4:
-        {
-        div = a1 / a2;
-        std::cout << "Quotient is " << div << std::endl;
-        break;
-        }
-    default:
-        std::cout << "Entered wrong action!" << std::endl;
-        break;
+    while (continueargument == 'y')
+    {
+        std::cout << "Choose action:" << std::endl << "Enter 1 for +" << std::endl << "Enter 2 for -" << std::endl << "Enter 3 for *" << std::endl << "Enter 4 for / " << std::endl;
+        std::cin >> operationtype;
+        std::cout << "Enter first number." << std::endl;
+        std::cin >> a1;
+        std::cout << "Eenter second number." << std::endl;
+        std::cin >> a2;
+            switch (operationtype) {
+            case 1: //making sum operation
+                {
+                sum = a1 + a2;
+                std::cout << "Sum is " << sum << std::endl;
+                break;
+                }
+            case 2: //making subtraction operation
+                {
+                razn = a1 - a2;
+                std::cout << "Difference is " << razn << std::endl;
+                break;
+                }
+            case 3: //making multiplication operation
+                {
+                pow = a1 * a2;
+                std::cout << "Product is " << pow << std::endl;
+                break;
+                }
+            case 4: // making division operation
+                {
+                div = a1 / a2;
+                std::cout << "Quotient is " << div << std::endl;
+                break;
+                }
+            default:
+                std::cout << "Entered wrong action!" << std::endl;
+                break;
+            }
+         std::cout << "Do you whant to make next calculation? Y/N" << std::endl;
+         std::cin >> continueargument;
     }
     return 0;
 }
